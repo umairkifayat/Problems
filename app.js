681423d4203eafea07764592f3c04b22ec5e55e6
umair kifayat <umairkifayat3@gmail.com>
2023-08-16 05:37:38 -0700

    problem

---
+++ app.js
@@ -0,0 +1,95 @@
+
+// question no 1 start
+// let num1 = 5;
+// let num2 = 8;
+ // console.log(5 + 8);
+
+// function user(num1,num2) {
+//     console.log(num1,num2);
+// }
+// user(num1,num2)
+ // question no 1 end
+
+
+// question no 2 start
+
+// let first_name = prompt("first name")
+// let last_name = prompt("last name")
+// console.log(first_name,last_name);
+
+// function user(first_name, last_name) {
+//     console.log(first_name + last_name);
+// }
+// user(first_name, last_name)
+// question no 2 end
+
+
+
+// question no 3 start
+
+// let num1 = +prompt("enter the first number")
+// let num2 = +prompt("enter the second number")
+// console.log(num1+num2);
+
+// function user(num1,num2) {
+//    console.log(num1,num2);
+// }
+// user(num1+num2)
+// question no 3 end
+
+
+
+
+// question no 4 end
+
+// let num1 = +prompt("enter the first number")
+//  let num2 = +prompt("enter the second number")
+//  let operators = prompt("Enter Operators")
+
+
+//  if (operators) {
+//     let sum = num1+num2
+//     console.log(num1+num2);
+//  }else;
+//  console.log("else sum k chl gya bahi");
+ 
+//  if (operators) {
+//     let add = num1*num2
+//     console.log(num1-num2);
+//  }else;
+//  console.log("else sum k chl gya bahi");
+ 
+//  if (operators) {
+//     let multiply = num1*num2
+//     console.log(num1*num2);
+//  }else;
+//  console.log("else sum k chl gya bahi");
+ 
+//  if (operators) {
+//     let divide = num1/num2
+//     console.log(num1/num2);
+//  }else;
+//  console.log("else sum k chl gya bahi");
+ 
+//  if (operators) {
+//     let modulus = num1%num2
+//     console.log(num1%num2);
+//  }else;
+//  console.log("else sum k chl gya bahi");
+
+
+
+
+
+
+
+// // question no 4 end
+
+
+
+ // question no 5 start
+
+
+// let user = +prompt("Enter the number");
+// console.log(user ** 2);
+ // question no 5 end
